1e6fec2e5d85a7273e6266b4f95fe1974bdcddb6
qxh711805 <1058626704@qq.com>
2019-09-30 17:32:19 +0800

    单向环形链表，经典约瑟夫问题。

---
+++ src/com/test/linkedlist/Jsosephu.java
@@ -0,0 +1,42 @@
+package com.test.linkedlist;
+
+
+/**
+ *  经典约瑟夫问题
+ *    环形链表
+ */
+public class Jsosephu {
+    public static void main(String[] args) {
+
+    }
+}
+
+class CircleSingleLinkedList{
+    //创建一个first节点，给一个默认值，没有编号
+    private Boy first=new Boy(-1);
+
+    //添加小孩节点，构建成一个环形链表
+    public void addBoy(int nums){
+
+    }
+
+
+}
+
+class Boy{
+    private int no;
+    private Boy next;
+
+    public Boy(int no) {
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+}
+
