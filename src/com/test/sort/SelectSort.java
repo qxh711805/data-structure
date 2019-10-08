456af4e9771f752d8e5bd69cfb24f722fc5b9ed7
qxh711805 <1058626704@qq.com>
2019-10-08 10:13:29 +0800

    选择排序

--- src/com/test/sort/SelectSort.java
+++ src/com/test/sort/SelectSort.java
@@ -9,22 +9,21 @@ import java.util.Date;
  */
 public class SelectSort {
     public static void main(String[] args) {
-        int sixe = 8;
-
+        int sixe = 80000;
         int arr[] = new int[sixe];
-
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 80000);
         }
         Date date = new Date();
-        System.out.println(date);
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String format = dateFormat.format(date);
         System.out.println("排序前时间" + format);
-
         selectSort(arr);
+        Date date2 = new Date();
+        String format2 = dateFormat.format(date2);
+        System.out.println("排序后时间" + format2);
 
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
     }
 
     private static void selectSort(int[] arr) {
