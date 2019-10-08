c327cbdccbf3dc39cdcba19b79104fb8902e7996
qxh711805 <1058626704@qq.com>
2019-10-08 10:09:50 +0800

    选择排序

--- src/com/test/sort/SelectSort.java
+++ src/com/test/sort/SelectSort.java
@@ -37,8 +37,40 @@ public class SelectSort {
                     minIndex = j;
                 }
             }
-            arr[minIndex] = arr[i];
-            arr[i] = min;
+            if (minIndex!=0){
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
         }
+
+
+        /*
+        思路
+        第一轮排序
+        int minIndex = 0;
+        int min = arr[0];
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                minIndex = j;
+            }
+        }
+        if (minIndex!=0){
+            arr[minIndex] = arr[0];
+            arr[0] = min;
+
+         第二轮排序
+        int minIndex = 1;
+        int min = arr[1];
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                minIndex = j;
+            }
+        }
+        if (minIndex!=0){
+            arr[minIndex] = arr[1];
+            arr[1] = min;
+        }*/
     }
 }
